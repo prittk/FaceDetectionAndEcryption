6357a0fd4a8b44d67f48231e128b15bbce73a5a8
prittk <114107523+prittk@users.noreply.github.com>
2025-04-07 13:10:57 -0400

    Update FaceTrack.py
    New features

--- FaceTrack.py
+++ FaceTrack.py
@@ -5,14 +5,32 @@ import math
 import hashlib
 from Crypto.Cipher import AES
 from Crypto.Util.Padding import pad, unpad
+#constantly used
+distanceUsedForKey=0
+leftUsedForKey=0
+rightUsedForKey=0
+#key saved to ecrypt
+encryptDist = distanceUsedForKey
+encryptLeft = leftUsedForKey
+encryptRight = rightUsedForKey
 
+#Text to be encrypted and deciphered
+plaintext = "Hello World"
+ciphertext = ""
+cipherDistAES=0 
 
+decrypt = False
 
 
 def openWebCam():
+    global decrypt
     webCam = cv2.VideoCapture(0) #open webcame
     detectFace = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
-    
+    cv2.namedWindow('frame')
+    switch = 'Encrypt 0 : OFF \n1 : ON'
+    switch2 = 'Decipher 0 : OFF \n1 : ON'
+    cv2.createTrackbar(switch, 'frame',0,1,encryptLandmarks)  #cv2 doesnt have button support through pip install, for ease of use for everyone i made a switch trackbar      
+    cv2.createTrackbar(switch2,'frame',0,1,startDecrypt)
     faces = [] #stores the unique data of the face
     i = 0
     while True:
@@ -20,11 +38,16 @@ def openWebCam():
         
         text_pane = np.zeros((100, frame.shape[1],3),dtype = np.uint8)
         
-        cv2.putText(text_pane, "This is the text pane!", (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
+        cv2.putText(text_pane, plaintext, (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
         
+       
         combined_frame = np.vstack((frame, text_pane))
         
-        cv2.imshow("Frame", combined_frame)
+        if decrypt:
+            print("Trying to decrypt")
+            decryptLandmarks()
+        
+        cv2.imshow("frame", combined_frame)
     
         k = cv2.waitKey(1)
         if k== ord('q'):
@@ -71,33 +94,43 @@ def getFaceData(x,y,w,h,gray,frame):
     
         # Draw landmarks on the frame of a circle for expected part detected
         for n in range(68):  # 68 landmarks
-            x_landmark_leftEye = landmarks.part(n).x
-            y_landmark_rightEye = landmarks.part(n).y
+            x_landmark = landmarks.part(n).x
+            y_landmark = landmarks.part(n).y
+            assert(landmarks.num_parts == 68)
+            drawPoints(frame, landmarks, 0, 16)
+            drawPoints(frame, landmarks, 36, 41, True)   #draws the blue line on face rec
+            drawPoints(frame, landmarks, 42, 47, True)
             
             
-            cv2.circle(frame, (x_landmark_leftEye, y_landmark_rightEye), 2, (0, 255, 0), -1)
+            cv2.circle(frame, (x_landmark, y_landmark), 2, (0, 255, 0), -1)
           
         left, right, distanceBetween =getEyeWidth(landmarks)
+        
         widthOfEye = [left,right]
         print(f"Left eye width {widthOfEye[0]}, Right eyes width {widthOfEye[1]}")
         
-        
+        #set the global variables to encrypt or decrypt
         distanceUsedForKey = distanceBetween #TODO: Use this to decrypt by saying if distance is within 10%
+        leftUsedForKey = left
+        rightUsedForKey = right
         
-        key = str(distanceBetween)
-        hashedKey = hashlib.sha256(key.encode()).digest()
-        
-        FirstBitskey = hashedKey[:32]
-        cipher = AES.new(FirstBitskey, AES.MODE_CBC)
-        print(f"Hashed key = {hashedKey}")
+
         
         
+def drawPoints(image, faceLandmarks, startpoint, endpoint, isClosed=False):
+  points = []
+  for i in range(startpoint, endpoint+1):
+    point = [faceLandmarks.part(i).x, faceLandmarks.part(i).y]
+    points.append(point)
+
+  points = np.array(points, dtype=np.int32)
+  cv2.polylines(image, [points], isClosed, (255, 200, 0), thickness=2, lineType=cv2.LINE_8)
 
         
         
        
   
-       
+       ##calculate distance
 def distanceCalc(point1,point2):
     
     return math.sqrt(((point1[0] - point2[0])**2) + ((point1[1] - point2[1])**2)) #sqrt((x2-x1)**2 + (y2-y1)**2)
@@ -106,18 +139,16 @@ def distanceCalc(point1,point2):
 def getEyeWidth(landmarks):
     ##left eye left point == 37 left eye right point == 40
     #https://www.studytonight.com/post/dlib-68-points-face-landmark-detection-with-opencv-and-python
-    leftOuter = (landmarks.part(37).x, landmarks.part(37).y) # 36 is the number for left eye outer
-    leftInner = (landmarks.part(40).x, landmarks.part(40).y) #39 inner
+    leftOuter = (landmarks.part(36).x, landmarks.part(36).y) # 36 is the number for left eye outer
+    leftInner = (landmarks.part(39).x, landmarks.part(39).y) #39 inner
     ##right eye left point == 43   right eyes right point == 46
-    rightInner = (landmarks.part(43).x, landmarks.part(43).y)
-    rightOuter = (landmarks.part(46).x, landmarks.part(46).y)
+    rightInner = (landmarks.part(42).x, landmarks.part(42).y)
+    rightOuter = (landmarks.part(45).x, landmarks.part(45).y)
     
     
-    face_width = math.sqrt((landmarks.part(1).x - landmarks.part(17).x)**2)  # Width across cheeks used for normalizing the image so distance doesnt affect result
-
-    
-    leftWidth = distanceCalc(leftOuter,leftInner)/face_width
-    rightWidth = distanceCalc(rightInner,rightOuter)/face_width
+    face_width = math.sqrt((landmarks.part(0).x - landmarks.part(16).x)**2)  # Width across cheeks used for normalizing the image so distance doesnt affect result
+    leftWidth = distanceCalc(leftOuter,leftInner)/face_width #left eye width fixed fro distance
+    rightWidth = distanceCalc(rightInner,rightOuter)/face_width #right eye width
     
     
     ##find distasnce between eye inners
@@ -126,6 +157,61 @@ def getEyeWidth(landmarks):
     
 
     return leftWidth, rightWidth, distanceBetweenBoth
+
+def encryptLandmarks(x):
+        global plaintext #access the global
+        global ciphertext
+        global cipherDistAES
+        global encryptDist
+        
+        encryptDist = distanceUsedForKey
+        encryptLeft = leftUsedForKey
+        encryptRight = rightUsedForKey
+        
+        keyDist = str(encryptDist)
+        keyLeft = str(encryptLeft)
+        keyRight = str(encryptRight)
+        
+        hashedKeyDist = hashlib.sha256(keyDist.encode()).digest()
+        hashedLeft = hashlib.sha256(keyLeft.encode()).digest()
+        hashedRight = hashlib.sha256(keyRight.encode()).digest()
+        #get 32 bytes
+        firstBitskeyDist = hashedKeyDist[:32]
+        firstBitsLeft = hashedLeft[:32] #0 - 31
+        firstBitsRight = hashedRight[:32]
+        
+        cipherDistAES = AES.new(firstBitskeyDist, AES.MODE_CBC)
+        cipherLeftAES = AES.new(firstBitsLeft,AES.MODE_CBC)
+        cipherRightAES = AES.new(firstBitsRight,AES.MODE_CBC)
+        plaintext_bytes = plaintext.encode() #have to make string byte form to encrypt
+        ciphertext = cipherDistAES.encrypt(pad(plaintext_bytes, AES.block_size)) #pad if needed
+        plaintext= str(ciphertext)
+        
+        
+        print(f"Hashed key Distance= {hashedKeyDist}")
+        
+def decryptLandmarks():
+    global cipherDistAES
+    global ciphertext
+    global encryptDist
+    global distanceUsedForKey
+    
+        #10 % greater or less match to decrypt
+    print(f"Distance key {distanceUsedForKey}")
+    print(f"Encrypted Distance {encryptDist}")
+    if (encryptDist- (encryptDist *.1 ))<= distanceUsedForKey <= ((encryptDist *.1 )+ encryptDist):
+        decrypt_cipher = AES.new(encryptDist, AES.MODE_CBC)
+        decryptedData = unpad(decrypt_cipher.decrypt(ciphertext), AES.block_size)
+        print("Decrypted Data:", decryptedData)
+
+        plaintext = decryptedData
+
+        #TODO: Add stuff to decrypt message
+    
+def startDecrypt(x):
+    global decrypt
+    decrypt = True
+        
 
 
 
